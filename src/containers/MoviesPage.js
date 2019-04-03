57518a95fa3066a44937af9ddd6fcdf8ca401b0d
carly <carly@noukod.com>
2019-04-03 13:58:18 +0000

    Automatically backed up by Learn

--- src/containers/MoviesPage.js
+++ src/containers/MoviesPage.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 import MoviesList from '../components/MoviesList';
 import MovieShow from './MovieShow';
- 
+
 const MoviesPage = ({ match, movies }) => (
   <div>
     <MoviesList movies={movies} />
@@ -12,5 +12,5 @@ const MoviesPage = ({ match, movies }) => (
     <Route path={`${match.url}/:movieId`} render={routerProps => <MovieShow movies={movies} {...routerProps} /> }/>
   </div>
 )
- 
+
 export default MoviesPage
